1dd08f66e66b313ee9020227aaa44d818f90deeb
Ushan Edirisinghe <30338490+malindaushan999@users.noreply.github.com>
2024-11-19 13:00:17 +0530

    Update HealthcareProcessTests.cs

--- VH.DotNetTeam.UnitTestDemo/Demo.UnitTests/HealthcareProcessTests.cs
+++ VH.DotNetTeam.UnitTestDemo/Demo.UnitTests/HealthcareProcessTests.cs
@@ -3,5 +3,19 @@ namespace Demo.UnitTests;
 [TestFixture]
 public class HealthcareProcessTests
 {
+    /// <summary>
+    /// The instance of the HealthcareProcess class that will be used in the tests.
+    /// </summary>
+    private HealthcareProcess _healthcareProcess;
+    
+    /// <summary>
+    /// Sets up the test environment by initializing the HealthcareProcess instance.
+    /// </summary>
+    [SetUp]
+    public void Setup()
+    {
+        _healthcareProcess = new HealthcareProcess();
+    }
+    
     // YOUR CODE SHOULD BE IN HERE
 }
